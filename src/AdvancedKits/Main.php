c3e88d12ea2221887e127896be6d57e2c5296f26
luca28pet <lucapetrucci28@yahoo.it>
2015-07-14 23:21:19 +0200

    Version 2.0

--- src/AdvancedKits/Main.php
+++ src/AdvancedKits/Main.php
@@ -2,148 +2,110 @@
 
 namespace AdvancedKits;
 
+use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\utils\Config;
+use pocketmine\tile\Sign;
 
 class Main extends PluginBase implements Listener{
 
-	public $hasKit = [];
+    private $kits;
+    private $hasKit = [];
 
-	/** @var Config*/
-	private $kits;
+    public function onEnable(){
+        @mkdir($this->getDataFolder());
+        if(!file_exists($this->getDataFolder()."kits.yml")){
+            $r = $this->getResource("kits.yml");
+            $o = stream_get_contents($r);
+            fclose($r);
+            file_put_contents($this->getDataFolder()."kits.yml", $o);
+        }
+        $this->kits = yaml_parse(file_get_contents($this->getDataFolder()."kits.yml"));
+    }
 
-	/** @var Config*/
-	public $vipPlayers;
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        switch(strtolower($command->getName())){
+            case "kit":
+                if(!($sender instanceof Player)){
+                    $sender->sendMessage("Please run this command in game");
+                    return true;
+                }
+                if(!isset($args[0])){
+                    $sender->sendMessage("Available kits: ".implode(", ", array_keys($this->kits)));
+                    return true;
+                }
+                if(isset($this->hasKit[$sender->getId()])){
+                    $sender->sendMessage("You already have a kit");
+                    return true;
+                }
+                if(!isset($this->kits[strtolower($args[0])])){
+                    $sender->sendMessage("Kit ".$args[0]." does not exist");
+                    return true;
+                }
+                if(!$sender->hasPermission("advancedkits.".strtolower($args[0]))){
+                    $sender->sendMessage("You haven't the permission to use kit ".$args[0]);
+                    return true;
+                }
+                $this->addKit(strtolower($args[0]), $sender);
+                $sender->sendMessage("Selected kit: ".$args[0]);
+                return true;
+            break;
+        }
+        return true;
+    }
 
-	/** @var Config*/
-	public $vipPlayersPlus;
+    public function onSign(PlayerInteractEvent $event){
+        $id = $event->getBlock()->getId();
+        if($id === Block::SIGN_POST or $id === Block::WALL_SIGN){
+            $tile = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+            if($tile instanceof Sign){
+                $text = $tile->getText();
+                if(trim($text[0]) === "[AdvancedKits]"){
+                    if(empty($text[1])){
+                        $event->getPlayer()->sendMessage("On this sign, the kit is not specified");
+                        return;
+                    }
+                    if(!isset($this->kits[strtolower($text[1])])){
+                        $event->getPlayer()->sendMessage("Kit ".$text[1]." does not exist");
+                        return;
+                    }
+                    if(!$event->getPlayer()->hasPermission("advancedkits.".strtolower($text[1]))){
+                        $event->getPlayer()->sendMessage("You haven't the permission to use kit ".$text[1]);
+                        return;
+                    }
+                    $this->addKit(strtolower($text[1]), $event->getPlayer());
+                }
+            }
+        }
+    }
 
-	public function onEnable(){
-  		@mkdir($this->getDataFolder());
-  		$this->kits = new Config($this->getDataFolder()."kits.yml", Config::YAML, [
-			"basicpvp" => ["Rank" => "Player", "Armor" => [1, 2, 3, 4], "Content" => [
-					[272, 0, 1],
-					[260, 0, 5],
-					[260, 0, 5]
-				]
-			],
-			"basicbuilder" => ["Rank" => "Player", "Armor" => [], "Content" => [
-                   	[4, 0, 25],
-                   	[275, 0, 1],
-                   	[297, 0, 3]
-                ]
-            ],
-			"darkgodpvp" => ["Rank" => "Vip", "Armor" => [], "Content" => [
-					[276, 0, 2],
-                  	[311, 0, 1],
-                   	[366, 0, 20]
-               	]
-           	]
-		]);
-  		$this->kits->save();
-  		$this->vipPlayers = new Config($this->getDataFolder()."vips.txt", Config::ENUM);
-  		$this->vipPlayersPlus = new Config($this->getDataFolder()."vips+.txt", Config::ENUM);
-  		$this->getServer()->getPluginManager()->registerEvents($this, $this);
- 	}
+    public function onDeath(PlayerDeathEvent $event){
+        if(isset($this->hasKit[$event->getEntity()->getId()])){
+            unset($this->hasKit[$event->getEntity()->getId()]);
+        }
+    }
 
- 	public function onDisable(){
- 	 	$this->kits->save();
- 	 	$this->vipPlayers->save();
- 	 	$this->vipPlayersPlus->save();
- 	}
+    public function onLogOut(PlayerQuitEvent $event){
+        if(isset($this->hasKit[$event->getPlayer()->getId()])){
+            unset($this->hasKit[$event->getPlayer()->getId()]);
+        }
+    }
 
- 	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-  		switch($command->getName()){
-  			case "advancedkits":
-				if(!(isset($args[0]))){
-					return false;
-				}
-				if($args[0] == "get"){
-					if(!(isset($args[1]))){
-						return false;
-					}
-					if(!($sender instanceof Player)){
-						$sender->sendMessage("Run this command in game.");
-						return true;
-					}
-					if(in_array($sender->getName(), $this->hasKit)){
-						$sender->sendMessage("[AdvancedKits] You already got a kit.");
-						return true;
-					}
-					if($this->kits->exists($args[1])){
-						$readconfig = $this->kits->get($args[1]);
-						$kit = new Kit($readconfig["Armor"], $readconfig["Content"], $readconfig["Rank"], $args[1], $this);
-						$kit->give($sender);
-						return true;
-					}else{
-						$sender->sendMessage("[AdvancedKits] Kit ".$args[1]." does not exist.");
-						return true;
-					}
-				}elseif($args[0] == "addvip"){
-					if($sender->isOp() or !($sender instanceof Player)){
-						if(!(isset($args[1]))){
-							return false;
-						}
-						if(isset($args[2]) and $args[2] == "plus"){
-							$this->vipPlayersPlus->set(strtolower($args[1]));
-							$this->vipPlayersPlus->save();
-							$sender->sendMessage("[AdvancedKits] ".$args[1]." has been added to vips +.");
-							return true;
-						}else{
-							$this->vipPlayers->set(strtolower($args[1]));
-							$this->vipPlayers->save();
-							$sender->sendMessage("[AdvancedKits] ".$args[1]." has been added to vips.");
-							return true;
-						}
-					}else{
-						$sender->sendMessage("[AdvancedKits] You need to be an OP in order to run this command.");
-						return true;
-					}
-				}elseif($args[0] == "unvip"){
-					if($sender->isOp() or !($sender instanceof Player)){
-						if(!(isset($args[1]))){
-							return false;
-						}
-						if(isset($args[2]) and $args[2] == "plus"){
-							$this->vipPlayersPlus->remove(strtolower($args[1]));
-							$this->vipPlayersPlus->save();
-							$sender->sendMessage("[AdvancedKits] ".$args[1]." has been removed from vips +.");
-							return true;
-						}else{
-							$this->vipPlayers->remove(strtolower($args[1]));
-							$this->vipPlayers->save();
-							$sender->sendMessage("[AdvancedKits] ".$args[1]." has been removed from vips.");
-							return true;
-						}
-					}else{
-						$sender->sendMessage("[AdvancedKits] You need to be an OP in order to run this command.");
-						return true;
-					}
-				}else{
-					return false;
-				}
-			break;
-			default:
-				return false;
-		}
-	}
-	
-	public function onDeath(PlayerDeathEvent $event){
-		if(($key = array_search($event->getEntity()->getName(), $this->hasKit)) !== false) {
-			unset($this->hasKit[$key]);
-		}
-	}
-	
-	public function onQuit(PlayerQuitEvent $event){
-		if(($key = array_search($event->getPlayer()->getName(), $this->hasKit)) !== false) {
-			unset($this->hasKit[$key]);
-		}
-	}
+    private function addKit($name, Player $player){
+        $items = $this->kits[$name]["items"];
+        $inv = $player->getInventory();
+        foreach($items as $item){
+            $itemData = array_map("intval", explode(":", $item));
+            $inv->setItem($inv->firstEmpty(), Item::get($itemData[0], $itemData[1], $itemData[2]));
+        }
+        $this->hasKit[$player->getId()] = true;
+    }
 
 }
