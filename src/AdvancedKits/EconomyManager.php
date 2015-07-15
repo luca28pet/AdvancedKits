b607fc04ff9a919335fba60f4ba2af39a74e7e01
luca28pet <lucapetrucci28@yahoo.it>
2015-07-15 12:37:45 +0200

    Added economy support

---
+++ src/AdvancedKits/EconomyManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AdvancedKits;
+
+use pocketmine\Player;
+
+class EconomyManager{
+
+    private $plugin;
+    private $economy = null;
+    private $api;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+        $plugins = ["EconomyAPI", "PocketMoney", "MassiveEconomy"];
+        foreach($plugins as $plugin){
+            $p = $this->plugin->getServer()->getPluginManager()->getPlugin($plugin);
+            if($p !== null){
+                $this->economy = $plugin;
+                $this->api = $p;
+                break;
+            }
+        }
+    }
+
+    public function grantKit(Player $player, $money){
+        if(is_null($this->economy)){
+            return false;
+        }
+        switch($this->economy){
+            case "EconomyAPI":
+                if($this->api->reduceMoney($player, $money) === 1){
+                    return true;
+                }
+            break;
+            case "PocketMoney":
+                if($this->api->getMoney($player->getName()) < $money){
+                    return false;
+                }
+                if($this->api->setMoney($player->getName(), $this->api->getMoney($player->getName()) - $money)){
+                    return true;
+                }
+            break;
+            case "MassiveEconomy":
+                if($this->api->takeMoney($player->getName(), $money) === 2){
+                    return true;
+                }
+            break;
+        }
+        return false;
+    }
+
+}
