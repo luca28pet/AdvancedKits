2236a41889c20a5b586883a2fdd57dc562bc3434
luca28pet <7382281+luca28pet@users.noreply.github.com>
2015-08-20 16:17:11 +0200

    4.0 The OOP Update!!!

---
+++ src/AdvancedKits/Kit.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace AdvancedKits;
+
+use pocketmine\item\Item;
+use pocketmine\Player;
+
+class Kit{
+
+    private $ak;
+    private $data;
+    private $name;
+    private $coolDowns = [];
+
+    public function __construct(Main $ak, array $data, $name){
+        $this->ak = $ak;
+        $this->data = $data;
+        $this->name = $name;
+        if(file_exists($this->ak->getDataFolder()."cooldowns/".strtolower($this->name).".sl")){
+            $this->coolDowns = unserialize(file_get_contents($this->ak->getDataFolder()."cooldowns/".strtolower($this->name).".sl"));
+        }
+    }
+
+    public function handleRequest(Player $player){
+        if($this->testPermission($player)){
+            if(!isset($this->coolDowns[strtolower($player->getName())])){
+                if($this->ak->getConfig()->get("one-kit-per-life") ? isset($this->ak->hasKit[strtolower($player->getName())]) : true){
+                    if($this->isPaid()){
+                        if($this->ak->economy->grantKit($player, $this->getCost())){
+                            $this->addTo($player);
+                            $player->sendMessage("Selected kit: ".$this->name);
+                        }else{
+                            $player->sendMessage("You cannot afford kit: ".$this->name);
+                        }
+                    }else{
+                        $this->addTo($player);
+                        $player->sendMessage("Selected kit: ".$this->name);
+                    }
+                }else{
+                    $player->sendMessage("You can only get one kit per life");
+                }
+            }else{
+                $player->sendMessage("Kit ".$this->name." is in coolDown at the moment");
+                $player->sendMessage("You will be able to get it in ".$this->getCoolDownLeft($player));
+            }
+        }else{
+            $player->sendMessage("You haven't the permission to use kit ".$this->name);
+        }
+    }
+
+    private function addTo(Player $player){
+        $items = $this->getItems();
+        $inv = $player->getInventory();
+        foreach($items as $type => $item){
+            if(is_int($type)) $inv->addItem($item);
+            elseif($type === "helmet")  $inv->setHelmet($item);
+            elseif($type === "chestplate") $inv->setChestplate($item);
+            elseif($type === "leggings") $inv->setLeggings($item);
+            elseif($type === "boots") $inv->setBoots($item);
+        }
+        $this->coolDowns[strtolower($player)] = $this->getCoolDownMinutes();
+    }
+
+    /**
+     * @return Item[]
+     */
+    private function getItems(){
+        $items = [];
+        foreach($this->data["items"] as $itemString){
+            $itemData = array_map("intval", explode(":", $itemString));
+            $items[] = Item::get($itemData[0], $itemData[1], $itemData[2]);
+        }
+        foreach(["helmet", "chestplate", "leggings", "boots"] as $armor){
+            if(isset($this->data[$armor])){
+                $armorItem = Item::get((int) $this->data[$armor]);
+                $items[$armor] = $armorItem;
+            }
+        }
+        return $items;
+    }
+
+    private function isPaid(){
+        return isset($this->data["money"]) and $this->data["money"] !== 0;
+    }
+
+    //Call isPaid() before !!!
+    private function getCost(){
+        return (int) $this->data["money"];
+    }
+
+    private function getCoolDownMinutes(){
+        $min = 0;
+        if(isset($this->data["cooldown"]["minutes"])){
+            $min += (int) $this->data["cooldown"]["minutes"];
+        }
+        if(isset($this->data["cooldown"]["hours"])){
+            $min += (int) $this->data["cooldown"]["hours"] * 60;
+        }
+        return $min;
+    }
+
+    private function getCoolDownLeft(Player $player){
+        if(($minutes = $this->coolDowns[strtolower($player->getName())]) < 60){
+            return $minutes." minutes";
+        }
+        if(($modulo = $minutes % 60) !== 0){
+            return floor($minutes / 60)." hours and ".$modulo." minutes";
+        }
+        return ($minutes / 60)." hours";
+    }
+
+    public function processCoolDown(){
+        foreach($this->coolDowns as $player => $min){
+            $this->coolDowns[$player] -= 1;
+            if($this->coolDowns[$player] === 0){
+                unset($this->coolDowns[$player]);
+            }
+        }
+    }
+
+    private function testPermission(Player $player){
+        return $this->ak->permManager ? $player->hasPermission("advancedkits.".strtolower($this->name)) : (
+            (isset($this->data["users"]) ? in_array(strtolower($player->getName()), $this->data["users"]) : true)
+            and
+            (isset($this->data["worlds"]) ? in_array(strtolower($player->getLevel()->getName()), $this->data["worlds"]) : true)
+        );
+    }
+
+    public function close(){
+        if(count($this->coolDowns) > 0){
+            file_put_contents($this->ak->getDataFolder()."cooldowns/".strtolower($this->name).".sl", serialize($this->coolDowns));
+        }
+    }
+
+}
